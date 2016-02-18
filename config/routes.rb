2373b54017d601a4c4b3385ca74e829aca41e215
Michael <michaeldbudnick@gmail.com>
2016-02-18 17:57:34 -0500

    Setup bower for building out Angular Auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'angular#angular'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
