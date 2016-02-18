5f3d52431290330c6f04b1c6973af71794a8719f
Michael <michaeldbudnick@gmail.com>
2016-02-18 17:21:57 -0500

    User Test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'User' do
-  before(:all) do
+  before(:each) do
     @user = FactoryGirl.create(:user)
   end
   after(:all) do
@@ -16,10 +16,22 @@ describe 'User' do
       @user.username = nil
       expect(@user).to_not be_valid
     end
-    it "needs an email"
-    it "needs a password"
-    it "needs a password at least 8 characters"
-    it "needs a matching password_confirmation"
+    it "needs an email" do
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end
+    it "needs a password" do
+      @user.password = nil
+      expect(@user).to_not be_valid
+    end
+    it "needs a password at least 8 characters" do
+      @user.password = "123467"
+      expect(@user).to_not be_valid
+    end
+    it "needs a matching password_confirmation" do
+      @user.password_confirmation = "not_matching"
+      expect(@user).to_not be_valid
+    end
 
 
 
