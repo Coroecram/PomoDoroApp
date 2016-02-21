3ed504ca53f4f0ce57383f9fbdba82fee7a92e38
Michael <michaeldbudnick@gmail.com>
2016-02-20 22:13:07 -0500

    ngtable working

--- app/assets/javascripts/home/mainController.js
+++ app/assets/javascripts/home/mainController.js
@@ -1,27 +1,57 @@
 angular.module('pomoDoro')
 .controller('mainController', [
-  'user',
   '$scope',
+  '$http',
+  '$filter',
+  'user',
   'Auth',
   'ngTableParams',
-  function($scope, user, Auth, ngTableParams) {
+  function($scope, $http, $filter, user, Auth, ngTableParams) {
     $scope.todos = user.todos;
     $scope.signedIn = Auth.isAuthenticated;
     Auth.currentUser().then(function (currentUser){
       $scope.user = currentUser;
+
+      $http.get('/users/' + currentUser.id + '/todos.json')
+       .success(function(data, status) {
+         $scope.data = data;
+
+         $scope.todoTable = new ngTableParams({
+             page: 1,
+             count: 3,
+             sorting: {
+                 completed_pomos: 'asc'
+             }
+         }, {
+            counts: [1,3,5,10],       
+             total: $scope.data.length,
+             getData: function($defer, params) {
+                 var orderedData = params.sorting() ?
+                                     $filter('orderBy')($scope.data, params.orderBy()) :
+                                     $scope.data;
+
+                 $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+             }
+           });
+       });
+
+      // $scope.todoTable = new ngTableParams({
+      //   page: 1,
+      //   count: 3
+      // }, {
+      //     getData: function ($defer, params) {
+      //       $http.get('/users/' + currentUser.id + '/todos.json',
+      //       {params: {
+      //                 pageNumber: params.page()-1,
+      //                 rangeStart: rangeStart,
+      //                 rangeStop: rangeStop
+      //       }}).success(function(data, status) {
+      //           params.total(data.length);
+      //           $defer.resolve(data);
+      //       })
+      //     }
+      // })
     });
-
-    // $scope.usersTable = new ngTableParams({
-    //             page: 1,
-    //             count: 10
-    //         }, {
-    //             total: $scope.users.length,
-    //             getData: function ($defer, params) {
-    //                 $scope.data = $scope.users.slice((params.page() - 1) * params.count(), params.page() * params.count());
-    //                 $defer.resolve($scope.data);
-    //             }
-    //         });
-
     $scope.addTodo = function() {
       if (!$scope.title || $scope.title === '' && $scope.user.id) { return };
         user.createTodo(
