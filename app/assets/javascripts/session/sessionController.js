5a5ed8fbd5732ec968f48884265b30242e99ad55
Michael <michaeldbudnick@gmail.com>
2016-02-24 17:35:51 -0500

    Bad login error hacky

--- app/assets/javascripts/session/sessionController.js
+++ app/assets/javascripts/session/sessionController.js
@@ -7,17 +7,27 @@ function($scope, $state, Auth){
   Auth.currentUser().then(function() {
     $state.go('home');
   });
+  $scope.failed = false;
+  $scope.errors = "";
   $scope.signin = function() {
+    var interval = setTimeout(function() {
+      $scope.failed = true;
+      $scope.errors = "Invalid Email or Password";
+    }, 0); // hackiness because function(error) never called
     Auth.login($scope.user).then(function(){
+      clearTimeout(interval);
       $state.go('home');
-    }, function(error){
+    }, function(error) {
       debugger
+      // never called under any circumstances
     });
   };
   $scope.signup = function() {
     Auth.register($scope.user).then(function(){
       $state.go('home');
     }, function(error){
+      $scope.failed = true;
+      debugger
       // have to parse error.data.errors
     });
   };
