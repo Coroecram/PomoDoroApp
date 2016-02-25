eac57d25895afb095f963d38ee4d3613f304854d
Michael <michaeldbudnick@gmail.com>
2016-02-24 19:20:22 -0500

    Remove debugger

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ActionController::Base
   def new_csrf
-    debugger
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery? && !current_user
     render nothing: true
   end
